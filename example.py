60fdfa3584d4072590d72f5f5c9842b74baf291d
laksha-s <21a53.laksha@sjec.ac.in>
2024-10-28 15:11:46 +0530

    Add

--- example.py
+++ example.py
@@ -1,1 +1,10 @@
+#learn how to print largest number in a list
+
+numbers = [5, 6 , 8, 7, 2, 1, 10]
+max = numbers[0]
+
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
 
